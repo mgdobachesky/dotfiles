d9683b1f4ba01b4d95bb9ec4af36e8d95e9db0a1
Michael Dobachesky <mgdobachesky@gmail.com>
2024-11-08 14:18:17 -0500

    nvim updates

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -23,6 +23,26 @@ return {
           enabled = true,
           color_mode = true,
         },
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
       }
     })
 
