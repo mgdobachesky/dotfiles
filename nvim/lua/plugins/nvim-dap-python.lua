e0c1353eeb3b1de671f60ac89c69d3adb461764c
Michael Dobachesky <mgdobachesky@gmail.com>
2024-11-22 13:17:35 -0500

    nvim debug docker python processes

--- nvim/lua/plugins/nvim-dap-python.lua
+++ nvim/lua/plugins/nvim-dap-python.lua
@@ -3,4 +3,32 @@ return {
   dependencies = {
     "mfussenegger/nvim-dap"
   },
+  ft = {"py"},
+  config = function()
+    require('dap-python').setup(vim.fn.stdpath("data") .. '/mason/packages/debugpy/venv/bin/python3')
+
+    -- Add script to start debugpy server/adapter in docker container
+    -- vim.fn.system({"${script}", ${script_args}})
+
+    local host = "0.0.0.0"
+    local debug_port = 5678
+
+    table.insert(require('dap').configurations.python, {
+      type = 'python',
+      request = 'attach',
+      name = "Docker Attached Python Debugger",
+      connect = {
+        port = tonumber(debug_port),
+        host = host,
+      },
+      cwd = vim.fn.getcwd(),
+      mode = "remote",
+      pathMappings = {
+        {
+          localRoot = vim.fn.getcwd(),
+          remoteRoot = "/usr/src/app",
+        },
+      },
+    })
+  end,
 }
