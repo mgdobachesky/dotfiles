c2e6c026d061bc31dbed9ceea8c56551114fb3d3
Michael Dobachesky <mgdobachesky@gmail.com>
2024-11-30 10:00:51 -0500

    Update DAP keybindings

--- nvim/lua/plugins/nvim-dap-ui.lua
+++ nvim/lua/plugins/nvim-dap-ui.lua
@@ -8,11 +8,7 @@ return {
   config = function()
     require("dapui").setup()
 
-    vim.keymap.set("n", "<Leader>dh", "<CMD>lua require('dapui').eval()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>du", "<CMD>lua require('dapui').open()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>dc", "<CMD>lua require('dapui').close()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>dw", "<CMD>lua require('dapui').float_element('watches', { enter = true })<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>ds", "<CMD>lua require('dapui').float_element('scopes', { enter = true })<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>dr", "<CMD>lua require('dapui').float_element('repl', { enter = true })<CR>", { desc = "" })
+    vim.keymap.set("n", "<Leader>ds", "<CMD>lua require('dapui').open()<CR>")
+    vim.keymap.set("n", "<Leader>dc", "<CMD>lua require('dapui').close()<CR>")
   end,
 }
--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -7,12 +7,11 @@ return {
 
   },
   config = function()
-    vim.keymap.set("n", "<Leader>da", "<CMD>lua require('dap').continue()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>db", "<CMD>lua require('dap').toggle_breakpoint()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>dd", "<CMD>lua require('dap').continue()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>di", "<CMD>lua require('dap').step_into()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>do", "<CMD>lua require('dap').step_out()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>dO", "<CMD>lua require('dap').step_over()<CR>", { desc = "" })
-    vim.keymap.set("n", "<Leader>dt", "<CMD>lua require('dap').terminate()<CR>", { desc = "" })
+    vim.keymap.set("n", "<Leader>dc", "<CMD>lua require('dap').continue()<CR>")
+    vim.keymap.set("n", "<Leader>db", "<CMD>lua require('dap').toggle_breakpoint()<CR>")
+    vim.keymap.set("n", "<Leader>dn", "<CMD>lua require('dap').step_over()<CR>")
+    vim.keymap.set("n", "<Leader>di", "<CMD>lua require('dap').step_into()<CR>")
+    vim.keymap.set("n", "<Leader>do", "<CMD>lua require('dap').step_out()<CR>")
+    vim.keymap.set("n", "<Leader>dt", "<CMD>lua require('dap').terminate()<CR>")
   end,
 }
