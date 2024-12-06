edf2eb633227ab2eed493e0c0dfb66980d8d2418
Michael Dobachesky <mgdobachesky@gmail.com>
2024-12-06 09:48:35 -0500

    Nvim updates

--- nvim/lua/plugins/telescope-fzf-native.lua
+++ nvim/lua/plugins/telescope-fzf-native.lua
@@ -1,4 +1,10 @@
 return {
   "nvim-telescope/telescope-fzf-native.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
   build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+  config = function()
+    require("telescope").load_extension("fzf")
+  end
 }
